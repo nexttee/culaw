f9e967fba64f1b71e652c043ac54a21050b24795
Michael Crawford <mcrawford@delphicdigital.com>
2017-06-07 09:23:48 -0400

    update sticky nav template. render the layout headline as the link label.

--- sites/all/themes/culaw/templates/paragraphs/paragraphs-item--sticky_navigation_card.tpl.php
+++ sites/all/themes/culaw/templates/paragraphs/paragraphs-item--sticky_navigation_card.tpl.php
@@ -1,6 +1,8 @@
 <?php
 //dpm($content);
 $count = 1;
+$label = "placeholder";
+$elements = array();
 ?>
 <div class="main-holder">
     <!-- sticky nav area -->
@@ -9,33 +11,35 @@ $count = 1;
             <ul class="sticky-nav list-unstyled">
 
                 <?php foreach($content['field_content_rows']['#items'] AS $key => $row) : ?>
-                    <li><a href="#section<?php print $count; ?>" class="smooth-anchor"><?php print $key; ?></a></li>
+                    <?php
+                    $pid = $row['value'];
+                    //$rid = $row['revision_id'];
+                    $entities = entity_load('paragraphs_item', array($pid));
+                    $temp = $entities;
+                    $entity = array_pop($temp);
+                    $label = $entity->field_headline['und'][0]['safe_value'];
+                    $paragraphs_render = entity_view('paragraphs_item', $entities, 'full');
+                    $elements[] = array(
+                            "output"=>drupal_render($paragraphs_render),
+                            "label"=>$label,
+                            "index"=>$count
+                    );
+                    ?>
+                    <li><a href="#section<?php print $count; ?>" class="smooth-anchor"><?php print $label; ?></a></li>
                     <?php $count++; ?>
                 <?php endforeach; ?>
 
             </ul>
         </div>
     </nav>
-    <?php $count = 1; ?>
-    <?php foreach($content['field_content_rows']['#items'] AS $key => $row) : ?>
-        <?php
-            $pid = $row['value'];
-            //$rid = $row['revision_id'];
-
-            $entities = entity_load('paragraphs_item', array($pid));
-            $paragraphs_render = entity_view('paragraphs_item', $entities, 'full');
-        ?>
+    <?php foreach($elements AS $key => $value) : ?>
 
         <div class="accordion-wrap">
-            <a href="#" class="accordion-opener">Curriculum</a>
-            <div class="three-column three-column__style03 accordion-slide" id="section<?php print $count; ?>">
-                <?php print drupal_render($paragraphs_render); ?>
+            <a href="#" class="accordion-opener"><?php print $value['label']; ?></a>
+            <div class="three-column three-column__style03 accordion-slide" id="section<?php print $value['index']; ?>">
+                <?php print $value['output']; ?>
             </div>
         </div>
-
-        <?php
-            $count++;
-        ?>
 
     <?php endforeach; ?>
 
